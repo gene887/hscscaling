d4a4d43e82108c4c08710eb31c4d7cd4f601d8b5
gene887 <109834717+gene887@users.noreply.github.com>
2026-08-27 12:05:24 +1000

    Add files via upload

---
+++ hscscalinggraphs/mx2.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from numpy.polynomial import Polynomial
+from sklearn.metrics import mean_absolute_error, r2_score
+
+def predict_hsc_mark(raw_mark):
+    df = pd.read_csv("mx2.csv")
+
+    x = np.array(df["Raw Mark"].tolist())
+    y = np.array(df["HSC Mark"].tolist())
+
+    model = Polynomial.fit(x, y, deg=2)
+
+    y_pred = model(x)
+
+    # mae and r^2
+    mae = mean_absolute_error(y, y_pred)
+    r2 = r2_score(y, y_pred)
+
+    print("MAE: ", mae)
+    print("R2: ", r2)
+
+    prediction = model(raw_mark)
+    print("Prediction: ", prediction)
+
+    # linspace provides evenly spaced numbers over a specified interval
+    curvy_line = np.linspace((df["Raw Mark"].min() // 10) * 10, 100, 1000)
+
+    #plotting
+    plt.title("Raw Mark to HSC Mark Prediction for Maths Ext 2")
+    plt.xlabel("Raw Mark")
+    plt.ylabel("HSC Mark")
+    plt.scatter(x, y, c='blue')
+    plt.plot(curvy_line, model(curvy_line), c='black', linewidth = 3, label="Prediction based off HSC Raw Marks Database")
+    plt.plot(curvy_line, curvy_line/2 + 50, c='green', linewidth = 2, linestyle = "dashed", label = "Divide by 2, Add 50")
+    plt.legend()
+    plt.show()
+
+# ENTER YOUR RAW MARK HERE
+predict_hsc_mark(82)
